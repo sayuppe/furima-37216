c328cc3b7dae26e49bdaa20fa3f539ae7515f9af
sayuppe <sk.purimo3@gmail.com>
2022-05-05 18:30:09 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,9 @@ class ItemsController < ApplicationController
   def index
     
   end
+
+  def item_params
+    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+
+
 end
