90793cfa7e1854ee7570e540ed6bc10cdb7becb2
sayuppe <sk.purimo3@gmail.com>
2022-05-06 01:31:32 +0900

    商品情報の入力まで

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,13 +1,24 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :genre
-
-  validates :title, :text, presence: true
-
-  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
-
-
   belongs_to :user
   has_one    :buy
   has_one_attached :image
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_cost
+  belongs_to :prefecture
+  belongs_to :shipping_days
+
+  validates :title, :text, presence: true
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_days_id, numericality: { other_than: 1 , message: "can't be blank"}
+  
+
+
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to:"items#index"
-  resources :items, only: [:index, :new]
+  root to: 'items#index'
+  resources :items, only: [:index, :new, :create]
+  resources :items
 end
