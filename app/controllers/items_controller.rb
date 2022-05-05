26a8c1f405e0a2abcfa8bee66d4e1125110e6154
sayuppe <sk.purimo3@gmail.com>
2022-05-05 21:11:56 +0900

    商品出品

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,19 @@
 class ItemsController < ApplicationController
-  def index
-    
+  class ItemsController < ApplicationController
+    def index
+      @items = Item.order("created_at DESC")
+    end
+  
+    def new
+      
+    end
+  
+    def item_params
+      params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+    end
+  
+  
+  
   end
-
-  def item_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
-
-
+  
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :genre
+
+  validates :title, :text, presence: true
+
+  validates :genre_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+
   belongs_to :user
   has_one    :buy
   has_one_attached :image
