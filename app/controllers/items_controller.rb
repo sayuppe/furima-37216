7300deec02599e94465ffb32d09dfd66b80f9ca4
sayuppe <sk.purimo3@gmail.com>
2022-05-07 18:47:30 +0900

    editページのユーザー規制

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -26,6 +26,10 @@ class ItemsController < ApplicationController
 
     def edit
       @item = Item.find(params[:id])
+      if @item.user_id == current_user.id && @item.order.nil?
+      else
+        redirect_to root_path
+      end
     end
 
     def update
