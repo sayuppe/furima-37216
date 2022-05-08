7e2b7ee591419f3cae9fedf197938ea734163b2d
sayuppe <sk.purimo3@gmail.com>
2022-05-08 20:32:12 +0900

    条件分岐表示

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,4 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :non_purchased_item, only: [:index, :create]
+
 
   def index
     @order_address = OrderAddress.new
@@ -11,11 +14,12 @@ class OrdersController < ApplicationController
 
   def create
     @order_address = OrderAddress.new(order_params)
+    @item = Item.find(params[:item_id])
     if @order_address.valid?
       @order_address.save
       redirect_to root_path
     else
-    #  render :index
+     render :index
     end
   end
 
@@ -24,6 +28,12 @@ class OrdersController < ApplicationController
 
   def order_params
    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id],token: params[:token])
+  end
+
+  def non_purchased_item
+    # itemがあっての、order_form（入れ子構造）。他のコントローラーで生成されたitemを使うにはcreateアクションに定義する。
+    @item = Item.find(params[:item_id])
+    redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
 
   
