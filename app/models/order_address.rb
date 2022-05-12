911b081adb387160ab9bf49d3fafa0e47736bb94
sayuppe <sk.purimo3@gmail.com>
2022-05-12 14:14:55 +0900

    バリデーションの修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -13,7 +13,7 @@ class OrderAddress < ApplicationRecord
       validates :phone_number, format: { with: /\A\d{10,11}\z/, message: 'is invalid' }
       validates :token
     end
-    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
     def save
       order = Order.create(user_id: user_id, item_id: item_id)
